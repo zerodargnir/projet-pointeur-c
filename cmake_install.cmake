328b5a108777fee7757c4cb3d8b5865907522684
zerodargnir <155982044+zerodargnir@users.noreply.github.com>
2024-03-29 15:53:00 +0100

    Update cmake_install.cmake

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/zero/cdev/listechainee
+
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/zero/cdev/listechainee/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE ${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
