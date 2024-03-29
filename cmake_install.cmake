d4675b7c632fc2f53e25cd6cb1802d1a24035da7
zerodargnir <155982044+zerodargnir@users.noreply.github.com>
2024-03-29 15:04:21 +0100

    Update cmake_install.cmake

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/ssor/cdev/listechainee
+# Install script for directory: /home/zero/cdev/listechainee
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
